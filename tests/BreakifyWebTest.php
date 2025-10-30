5cfed7ece52f637ee3bac8eaacdb813dbd79f866
Amit Joshi <amitjoc@gmail.com>
2025-10-30 20:06:38 +0530

    test(breakifyCliWeb): add breakifyWeb Test cases
    added breakify web test cases

--- tests/BreakifyWebTest.php
+++ tests/BreakifyWebTest.php
@@ -3,7 +3,6 @@
 namespace AJ\LineBreak\Tests;
 
 use AJ\LineBreak\Breakify;
-use PHPUnit\Framework\MockObject\MockClass;
 use PHPUnit\Framework\TestCase;
 
 class BreakifyWebTest extends TestCase
@@ -23,11 +22,47 @@ class BreakifyWebTest extends TestCase
         $this->breakifyMockObject->expects($this->once())
             ->method('isCliEnv')
             ->willReturn(false);
-        $this->assertEquals('web', $this->breakifyMockObject->checkRequest());
+        $this->assertEquals('web', $this->breakifyMockObject->exeEnvType());
     }
 
     public function testPLineBreakForWebBrTag()
     {
-        $this->assertEquals('<br />', $this->breakifyMockObject->pLineBreak());
+        $this->assertEquals('<br />', $this->breakifyMockObject->getLineBreak());
+    }
+
+    public function testPrintWebBrTag()
+    {
+        $this->expectOutputString('<br />');
+        $this->breakifyMockObject->pbr();
+    }
+
+    public function testPrintHrTag()
+    {
+        $this->expectOutputString('<hr />');
+        $this->breakifyMockObject->phr();
+    }
+
+    public function testPrintDashedHrTag()
+    {
+        $this->expectOutputString("<hr style='border-style: dashed' />");
+        $this->breakifyMockObject->phrDashed();
+    }
+
+    public function testPrintDottedHrTag()
+    {
+        $this->expectOutputString("<hr style='border-style: dotted' />");
+        $this->breakifyMockObject->phrDotted();
+    }
+
+    public function testPrintDoubledHrTag()
+    {
+        $this->expectOutputString("<hr style='border-style: double' />");
+        $this->breakifyMockObject->phrDouble();
+    }
+
+    public function testPrintRidgeHrTag()
+    {
+        $this->expectOutputString("<hr style='border-style: ridge' />");
+        $this->breakifyMockObject->phrRidge();
     }
 }
